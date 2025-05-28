b4f95bb3b0f2844747b789aab58facbd7dff2cbb
adityadeshpande0 <adityadeshpande1@outlook.com>
2025-05-29 01:20:06 +0530

    new api added

---
+++ controllers/get-profile-details/getProfileDetails.js
@@ -0,0 +1,19 @@
+const User = require("../../models/User");
+
+const getProfileDetails = async (req, res) => {
+  try {
+    const userId = req.user && req.user.id;
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    const user = await User.findById(userId).select("-authentication");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json({ user });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+module.exports = getProfileDetails;
--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const { registerUser } = require("../controllers/auth/registerController");
 const { loginUser } = require("../controllers/auth/loginController");
+const getProfileDetails = require("../controllers/get-profile-details/getProfileDetails");
+const authenticateToken = require("../middlewares/authenticateToken");
 const router = express.Router();
 
 //POST calls
 router.post("/register", registerUser);
-router.post('/login', loginUser);
+router.post("/login", loginUser);
 //GET calls
-
+router.get("/user-profile", authenticateToken, getProfileDetails);
 //PUT calls
 
 //DEL calls
